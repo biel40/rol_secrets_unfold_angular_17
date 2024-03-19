a8ab9e5ce77e0286ee038bc3eb75ebb28133aab0
biel40 <bielet40@gmail.com>
2024-03-19 21:44:13 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'rol_secrets_unfold_angular_17';
+}
