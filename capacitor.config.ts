f063525db78e99526929c15879895d3531c9e3cc
Gabriel Borrás Serra <biel40@Air-de-Gabriel.home>
2024-08-10 14:59:56 +0200

    added ios to capacitor and generates ios app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'rol_secrets_unfold_angular_17',
-  webDir: 'dist/rol_secrets_unfold_angular_17'
+  webDir: 'dist/rol_secrets_unfold_angular_17/browser'
 };
 
 export default config;
+ 
